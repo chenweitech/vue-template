f03284027496009e6108cff4e8c7cb3b586780bd
chenweitech <chenweitech@outlook.com>
2019-04-18 10:39:19 +0800

    feat: 增加demo样例

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <p>Hello Vue</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+#app {
+    text-align: center;
+    color: #376956;
+}
+</style>
